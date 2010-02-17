09fc8946dbe60f6d7bc3300d6c2220f9f593f9d0
bruno <bruno@pobox.com>
2010-02-17 14:37:32 +0100

    now the weather compiles

--- src/mimodek/Weather.java
+++ src/mimodek/Weather.java
@@ -1,16 +1,7 @@
+package mimodek;
+
 import processing.core.*; 
 import processing.xml.*; 
-
-import java.applet.*; 
-import java.awt.*; 
-import java.awt.image.*; 
-import java.awt.event.*; 
-import java.io.*; 
-import java.net.*; 
-import java.text.*; 
-import java.util.*; 
-import java.util.zip.*; 
-import java.util.regex.*; 
 
 public class Weather {
 
@@ -28,18 +19,16 @@ public class Weather {
 
 	public boolean update() {
 		try {
-			xml = new XMLElement(this, "http://rss.wunderground.com/auto/rss_full/global/stations/08221.xml?units=metric");
+			xml = new XMLElement("http://rss.wunderground.com/auto/rss_full/global/stations/08221.xml?units=metric");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		if (xml != null) {
-			int numKids = xml.getChildCount();
 			XMLElement firstItem = xml.getChild(0).getChild(11).getChild(1);
-			String name = firstItem.getName();
 			String content = firstItem.getContent();
-			String[] conditionsA = split(content, ':');
-			String[] conditionsB = split(conditionsA[1], 'C');
+			String[] conditionsA = processing.core.PApplet.split(content, ':');
+			String[] conditionsB =  processing.core.PApplet.split(conditionsA[1], 'C');
 			_temperature = PApplet.parseFloat (conditionsB[0]);
 			return true;
 		} else {
